e4c6c74b66af41bac6d2d2aa829f4dc537a59905
Jalaj Agarwal <Jalaj@Jalajs-MacBook-Pro.local>
2020-10-07 17:56:35 +1100

    first comment

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,20 +61,18 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Research papers at your fingertips!
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Have you ever gone through a Research paper which is 30-40 pages long and was unable to understand it? If yes, then you have come to right place. With this tool you can read an entire reserach paper in 10-15 minutes.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
                     Get started
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
-                    </Button>
+                  
                 </ButtonGroup>
               </div>
             </div>
